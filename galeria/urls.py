586f1ad64f5c0edc15811f6c5f320971cc1cd418
Bruno <brunocosta.1991@hotmail.com>
2023-04-16 11:23:39 -0300

    static files

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from galeria.views import index
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 ]
